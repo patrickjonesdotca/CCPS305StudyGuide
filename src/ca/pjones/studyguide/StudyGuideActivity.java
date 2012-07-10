1d2dde714df7685fa5cea5c94f31e89c7a9ce129
Patrick Jones <patrick@patrickjones.ca>
2012-07-10 01:21:20 -0400

    Classes 2-5

--- src/ca/pjones/studyguide/StudyGuideActivity.java
+++ src/ca/pjones/studyguide/StudyGuideActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -23,6 +24,16 @@ public class StudyGuideActivity extends ListActivity {
 			"Rabin–Karp_string_search_algorithm", "Recursion_(computer_science)", "Spaghetti_stack", "String_matching",
 			"Tail_recursion"
 	};
+	String[] class2 = {"Divide_and_conquer_algorithm", "Memoization", "Dynamic_programming", "Backtracking", "Look_ahead", "Greedy_algorithm" };
+	String[] class3 = {"Buffer_overflow", "Dangling_pointer", "Free_list", "Function_pointer", "Garbage_collection_(computer_science)",
+			"Malloc", "Memory_leak", "Opaque_pointer", "Pointer_(computer_programming)", "Reference_counting", "Smart_pointer"
+	};
+	String[] class4 = {"Dancing_links", "Disjoin_set_data_structure", "Doubled_ended_queue", "Linked_list", "Memory_pool",
+			"Queue_(data_structure)", "Stack_(data_structure)"
+	};
+	String[] class5 = {"Binomial_heap", "Buddy_memory_allocation", "Cycle_detection", "Dynamic_memory_allocation",
+			"Fragmentation_(computer)", "Rope_(computer_science)", "Tombstone_(programming)", "Trie"
+	};
 	HashMap<Integer, String[]> hp = new HashMap<Integer, String[]>();
 	
     /** Called when the activity is first created. */
@@ -30,6 +41,10 @@ public class StudyGuideActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         hp.put(0, class1);
+        hp.put(1, class2);
+        hp.put(2, class3);
+        hp.put(3, class4);
+        hp.put(4, class5);
         setUpList(classes);
     }
     
@@ -45,6 +60,16 @@ public class StudyGuideActivity extends ListActivity {
     	  launchViewer("class" + (Integer.toString(lastClassNum)) + "/" + lastSelected[position]);
       }
       super.onListItemClick(l, v, position, id);
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+        	classSelected = false;
+        	setUpList(classes);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
     
     private void setUpList(String[] elements) {
